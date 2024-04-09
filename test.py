3a0e79f3a5f610bfd49ef514bb92436f42a221b8
LvsalnNarayana <88507292+LvsalnNarayana@users.noreply.github.com>
2024-04-09 18:43:37 +0100

    python practice initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+# def myFunction():    print ('The value of __name__ is ' + __name__)
+# def main():    myFunction()
+# if __name__ == '__main__':    main()
+
+import test_module as te
+
+print(te.test_func())
---
+++ test_module.py
@@ -0,0 +1,5 @@
+def test_func ():
+    if __name__ == "__main__":
+         print("hello")
+       
+    return __name__
